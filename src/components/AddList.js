258981533924f46243350396c24521d197c2e2e1
Blackmile <stanley.ezeifeanyi@gmail.com>
2022-05-01 12:07:28 +0100

    adding task

--- src/components/AddList.js
+++ src/components/AddList.js
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, } from 'react-native'
+import React from 'react';
 
-const AddList = () => {
+const AddList = (props) => {
   return (
-    <View>
-      <Text>AddList</Text>
+    <View style={styles.container}>
+      <Text style={{color: '#fff'}} >{props.text}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f3f3f3',
+    padding: 20,
+    margin: 10,
+    width: 300,
+    height: 250,
+    alignContent: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  }
+})
 
 export default AddList
