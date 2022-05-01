82c7795c0cca05d503e3ace9e70c9dd6c5953a82
Blackmile <stanley.ezeifeanyi@gmail.com>
2022-05-01 12:06:10 +0100

    fix reanimation

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import AddList from '../components/AddList'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View>
+        <Text>Today's Task</Text>
+        <View>
+          <AddList text={'task 1'} />
+          <AddList text={'task 2'} />
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#971FCC',
+  }
+})
 
 export default Home
