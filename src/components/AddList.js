72cd3d50387b646a4f14bf5b11806787c09ca6ac
Blackmile <stanley.ezeifeanyi@gmail.com>
2022-05-02 10:40:09 +0100

    added button

--- src/components/AddList.js
+++ src/components/AddList.js
@@ -1,10 +1,24 @@
-import { View, Text, StyleSheet, } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import React from 'react';
+
+const handlePress = () => {
+  return alert('are you sure you want to delete this!?')
+} 
 
 const AddList = (props) => {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#fff'}} >{props.text}</Text>
+      <Text style={styles.primary} >{props.text}</Text>
+      <Text>Time: {props.time} </Text>
+      <View style={styles.bin}>
+        <TouchableOpacity>
+          <AntDesign name="edit" size={16} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <AntDesign name="delete" size={16} color="black" onPress={handlePress} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -15,11 +29,20 @@ const styles = StyleSheet.create({
     padding: 20,
     margin: 10,
     width: 300,
-    height: 250,
-    alignContent: 'center',
+    height: 150,
     justifyContent: 'center',
-    flexDirection: 'column',
-  }
+    borderRadius: 20,
+  },
+  primary: {
+    color: 'black',
+    fontSize: 24,
+  },
+  bin: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+  } 
 })
 
 export default AddList
